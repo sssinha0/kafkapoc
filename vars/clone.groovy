de81d602969f8c81de60bc62a1c0865e5f8a580a
sssinha0 <b180043@nitsikkim.ac.in>
2025-08-20 11:38:17 +0530

    test

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,4 +1,4 @@
-def call(String url, String branch){
+def call(){
     // git url "${url}", branch: "${branch}"
-    echo "Cloning from ${url} on branch ${branch}"
+    echo "Cloning from Hlleo from testing"
 }
